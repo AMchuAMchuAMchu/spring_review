a235b80fd12b74bbba8ed6887c21227e5b6f8f3e
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-20 08:47:08 +0800

    AM 01st commit

--- spring_first_01/src/main/java/com/itheima/dao/AnimeInfo.java
+++ spring_first_01/src/main/java/com/itheima/dao/AnimeInfo.java
@@ -1,6 +1,7 @@
 package com.itheima.dao;
 
 import com.itheima.utils.DateTimeUtils;
+import lombok.Data;
 
 /**
  * Description ==> TODO
@@ -10,18 +11,20 @@ import com.itheima.utils.DateTimeUtils;
  * CreateTime ==> 2022-10-20 08:33:18
  * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
  */
+@Data
 public class AnimeInfo {
 
-    private DateTimeUtils dateTimeUtils = null;
+    private DateTimeUtils dateTimeUtils;
 
     public void sayHello(){
-
         System.out.println("sayHello...");
         System.out.println("sayKNCW...");
         System.out.println("say(｡･∀･)ﾉﾞ嗨...");
         System.out.println("阔你吃哇~~当前时间是:>>"+ dateTimeUtils.getCurrentTime());
         System.out.println("阔你吃哇~~当前时间是:>>"+ dateTimeUtils.getCurrentTime());
     }
+
+
 
 
 }

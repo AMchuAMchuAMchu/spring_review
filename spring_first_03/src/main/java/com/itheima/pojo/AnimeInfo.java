0df722ca223e1b4e0886a2383d0955ee6bef84ea
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-21 18:47:05 +0800

    AM 01st commit

---
+++ spring_first_03/src/main/java/com/itheima/pojo/AnimeInfo.java
@@ -0,0 +1,18 @@
+package com.itheima.pojo;
+
+import lombok.Data;
+
+/**
+ * Description ==> TODO
+ * BelongsProject ==> spring_review
+ * BelongsPackage ==> com.itheima.pojo
+ * Version ==> 1.0
+ * CreateTime ==> 2022-10-21 18:43:14
+ * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+ */
+@Data
+public class AnimeInfo {
+
+
+
+}
---
+++ spring_first_03/src/test/java/AnimeInfoTest01.java
@@ -0,0 +1,31 @@
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.DruidPooledConnection;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.sql.SQLException;
+
+/**
+ * Description ==> TODO
+ * BelongsProject ==> spring_review
+ * BelongsPackage ==> PACKAGE_NAME
+ * Version ==> 1.0
+ * CreateTime ==> 2022-10-21 18:45:57
+ * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+ */
+public class AnimeInfoTest01 {
+
+    @Test
+    public void testDruid() throws SQLException {
+
+        ClassPathXmlApplicationContext cp = new ClassPathXmlApplicationContext("SpringConfig.xml");
+
+
+        DruidDataSource bean = cp.getBean(DruidDataSource.class);
+
+        DruidPooledConnection connection = bean.getConnection();
+        System.out.println(connection);
+
+    }
+
+}

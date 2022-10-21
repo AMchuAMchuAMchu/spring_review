f44078d6f860f4ce1ddf2597f0918c0ec1a0cb3b
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-21 17:37:35 +0800

    AM 01st commit

--- spring_first_01/src/main/java/com/itheima/dao/AnimeInfo.java
+++ spring_first_01/src/main/java/com/itheima/dao/AnimeInfo.java
@@ -2,6 +2,8 @@ package com.itheima.dao;
 
 import com.itheima.utils.DateTimeUtils;
 import lombok.Data;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 
 /**
  * Description ==> TODO
@@ -12,7 +14,7 @@ import lombok.Data;
  * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
  */
 @Data
-public class AnimeInfo {
+public class AnimeInfo implements InitializingBean, DisposableBean {
 
     private DateTimeUtils dateTimeUtils;
 
@@ -24,13 +26,23 @@ public class AnimeInfo {
         System.out.println("阔你吃哇~~当前时间是:>>"+ dateTimeUtils.getCurrentTime());
     }
 
-    public void init(){
-        System.out.println("inti ...");
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("destroy...");
     }
 
-    public void destroy(){
-        System.out.println("destroy ...");
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("init...");
     }
+//
+//    public void init(){
+//        System.out.println("inti ...");
+//    }
+//
+//    public void destroy(){
+//        System.out.println("destroy ...");
+//    }
 
 
 

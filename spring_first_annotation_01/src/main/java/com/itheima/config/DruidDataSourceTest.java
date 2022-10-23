12e0cf645688d80b6d5c69eb2fcb5be81a0473c8
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-23 12:08:16 +0800

    AM 01st commit

--- spring_first_annotation_01/src/main/java/com/itheima/config/DruidDataSourceTest.java
+++ spring_first_annotation_01/src/main/java/com/itheima/config/DruidDataSourceTest.java
@@ -1,6 +1,7 @@
 package com.itheima.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
@@ -15,18 +16,27 @@ import javax.sql.DataSource;
  * CreateTime ==> 2022-10-23 11:52:26
  * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
  */
-@Component
 @PropertySource("classpath:jdbc.properties")
 public class DruidDataSourceTest {
+
+
+    @Value("${jdbc.driverClassName}")
+    private String driverClassName;
+    @Value("${jdbc.url}")
+    private String url;
+    @Value("${jdbc.username}")
+    private String username;
+    @Value("${jdbc.password}")
+    private String password;
 
     @Bean
     public DataSource getDruidDataSourceTest(){
         DruidDataSource druidDataSource = new DruidDataSource();
 
-        druidDataSource.setDriverClassName("${jdbc.driverClassName}");
-        druidDataSource.setUrl("${jdbc.url}");
-        druidDataSource.setUsername("${jdbc.username}");
-        druidDataSource.setPassword("${jdbc.password}");
+        druidDataSource.setDriverClassName(driverClassName);
+        druidDataSource.setUrl(url);
+        druidDataSource.setUsername(username);
+        druidDataSource.setPassword(password);
 
         return druidDataSource;
     }

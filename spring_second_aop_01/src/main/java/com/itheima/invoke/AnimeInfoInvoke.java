cae4eab2693f1f9be0cc3aee796891ca77fe7536
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-24 14:43:12 +0800

    AM 01st commit

--- spring_second_aop_01/src/main/java/com/itheima/invoke/AnimeInfoInvoke.java
+++ spring_second_aop_01/src/main/java/com/itheima/invoke/AnimeInfoInvoke.java
@@ -3,7 +3,6 @@ package com.itheima.invoke;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 

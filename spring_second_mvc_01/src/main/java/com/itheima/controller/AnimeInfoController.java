4cb005ab1f0a0d7c864f043366849e8f8b2d23e8
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-26 11:59:32 +0800

    AM

--- spring_second_mvc_01/src/main/java/com/itheima/controller/AnimeInfoController.java
+++ spring_second_mvc_01/src/main/java/com/itheima/controller/AnimeInfoController.java
@@ -1,5 +1,12 @@
 package com.itheima.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.SocketTimeoutException;
+
 /**
  * Description ==> TODO
  * BelongsProject ==> spring_review
@@ -8,5 +15,16 @@ package com.itheima.controller;
  * CreateTime ==> 2022-10-26 10:57:16
  * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
  */
+@RequestMapping("/animeInfo")
+@RestController
 public class AnimeInfoController {
+
+    @GetMapping
+    public String getAnimeInfo(){
+        System.out.println("Lycoris recoil");
+        return "Lycoris recoil";
+    }
+
+
+
 }

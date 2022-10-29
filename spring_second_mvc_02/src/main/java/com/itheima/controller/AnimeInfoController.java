6f62090a0120cbda958b6f9292ca780e4e195a46
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-29 18:07:22 +0800

    AM

--- spring_second_mvc_02/src/main/java/com/itheima/controller/AnimeInfoController.java
+++ spring_second_mvc_02/src/main/java/com/itheima/controller/AnimeInfoController.java
@@ -77,7 +77,7 @@ public class AnimeInfoController {
 
     @GetMapping("/parameter07")
 //    public List<String> getList01(@RequestParam("animes") List<String> animes) {
-    public ArrayList<String> getList01(List<String> animes) {
+    public ArrayList<String> getList01(ArrayList<String> animes) {
         System.out.println("animes >> " + animes);
         ArrayList<String> animeList = new ArrayList<>();
         animeList.add("kirito");

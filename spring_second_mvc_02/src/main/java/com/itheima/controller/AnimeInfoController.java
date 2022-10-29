040dfe54bc483d4f989c9bf28c10b457b61f0f16
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-29 17:26:38 +0800

    AM

---
+++ spring_second_mvc_02/src/main/java/com/itheima/controller/AnimeInfoController.java
@@ -0,0 +1,30 @@
+package com.itheima.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Description ==> TODO
+ * BelongsProject ==> spring_review
+ * BelongsPackage ==> com.itheima.controller
+ * Version ==> 1.0
+ * CreateTime ==> 2022-10-29 17:20:54
+ * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+ */
+@RequestMapping("/animeInfo")
+@RestController
+public class AnimeInfoController {
+
+    @GetMapping
+    public String getString(String name,Integer time){
+        System.out.println("name >> "+name+"|| time >> "+time);
+        return "getString...Lycoris recoil";
+    }
+
+
+
+
+}
---
+++ spring_second_mvc_02/src/main/java/com/itheima/pojo/AnimeInfo.java
@@ -0,0 +1,21 @@
+package com.itheima.pojo;
+
+import lombok.Data;
+
+/**
+ * Description ==> TODO
+ * BelongsProject ==> spring_review
+ * BelongsPackage ==> com.itheima.pojo
+ * Version ==> 1.0
+ * CreateTime ==> 2022-10-29 17:20:10
+ * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+ */
+@Data
+public class AnimeInfo {
+
+    private String name;
+
+    private Integer time;
+
+
+}

92920c8e76f9b5016aa81be815320c4a0ecca67a
AMchuAMchuAMchu <2466130840@qq.com>
2022-11-01 10:08:22 +0800

    AM

---
+++ spring_third_mybatis-plus/src/test/java/com/itheima/SpringThirdMybatisPlusApplicationTests.java
@@ -0,0 +1,29 @@
+package com.itheima;
+
+import com.itheima.dao.AnimeInfoDao;
+import com.itheima.pojo.AnimeInfo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class SpringThirdMybatisPlusApplicationTests {
+
+
+    @Autowired
+    private AnimeInfoDao animeInfoDao;
+
+    @Test
+    void contextLoads() {
+
+        List<AnimeInfo> animeInfos = animeInfoDao.selectList(null);
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        animeInfos.forEach(System.out::println);
+
+    }
+
+}

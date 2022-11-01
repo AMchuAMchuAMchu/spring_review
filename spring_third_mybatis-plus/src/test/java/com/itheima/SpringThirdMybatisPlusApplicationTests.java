5def2e171b64eae8dcdf52f24ddc01eedfbf6e8a
AMchuAMchuAMchu <2466130840@qq.com>
2022-11-01 10:20:59 +0800

    AM

--- spring_third_mybatis-plus/src/test/java/com/itheima/SpringThirdMybatisPlusApplicationTests.java
+++ spring_third_mybatis-plus/src/test/java/com/itheima/SpringThirdMybatisPlusApplicationTests.java
@@ -19,12 +19,22 @@ class SpringThirdMybatisPlusApplicationTests {
     void contextLoads() {
 
 //        List<AnimeInfo> animeInfos = animeInfoDao.selectList(null);
-        AnimeInfo animeInfo = animeInfoDao.selectById(1);
+//        AnimeInfo animeInfo = animeInfoDao.selectById(1);
+        AnimeInfo animeInfo = new AnimeInfo();
+        animeInfo.setName("在地下城寻求邂逅是否搞错了什么...");
+        animeInfo.setReleaseTime(2015);
+        animeInfo.setCharacter02("赫斯提亚");
+        animeInfo.setCharacter01("贝尔克朗尼");
+        animeInfo.setId(39);
+
+        int insert = animeInfoDao.insert(animeInfo);
         System.out.println();
         System.out.println();
         System.out.println();
 //        animeInfos.forEach(System.out::println);
-        System.out.println(animeInfo);
+//        System.out.println(animeInfo);
+        System.out.println(insert>0);
+
 
     }
 

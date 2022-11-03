cc0aa2ae1bbf519cfebcf5ea594fd045bbfde8ce
AMchuAMchuAMchu <2466130840@qq.com>
2022-11-03 08:39:54 +0800

    shino_hecate

---
+++ spring_third_mybatis-plus/src/main/java/com/itheima/config/CFOptimistic.java
@@ -0,0 +1,29 @@
+package com.itheima.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Description ==> TODO
+ * BelongsProject ==> spring_review
+ * BelongsPackage ==> com.itheima.config
+ * Version ==> 1.0
+ * CreateTime ==> 2022-11-03 08:38:46
+ * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+ */
+@Configuration
+public class CFOptimistic {
+
+
+    @Bean
+    public MybatisPlusInterceptor getMPI(){
+        MybatisPlusInterceptor mpi = new MybatisPlusInterceptor();
+
+        mpi.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+
+        return mpi;
+    }
+
+}

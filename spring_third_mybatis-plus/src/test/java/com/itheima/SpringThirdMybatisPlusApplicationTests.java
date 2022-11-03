97381761993980ffc2a6c2cedf2dd94e806506e0
AMchuAMchuAMchu <2466130840@qq.com>
2022-11-03 08:44:34 +0800

    shino_hecate

--- spring_third_mybatis-plus/src/test/java/com/itheima/SpringThirdMybatisPlusApplicationTests.java
+++ spring_third_mybatis-plus/src/test/java/com/itheima/SpringThirdMybatisPlusApplicationTests.java
@@ -29,17 +29,19 @@ class SpringThirdMybatisPlusApplicationTests {
     @Test
     void testVersion(){
 
+        AnimeInfo01 animeInfo02 = new AnimeInfo01();
+        animeInfo02.setName01("地下城克朗尼02");
+        animeInfo02.setId(39);
+        animeInfo02.setVersion(1);
+        int i2 = animeInfo01Dao.updateById(animeInfo02);
+
+
         AnimeInfo01 animeInfo01 = new AnimeInfo01();
-        animeInfo01.setName01("地下城克朗尼");
+        animeInfo01.setName01("地下城克朗尼01");
         animeInfo01.setId(39);
-        animeInfo01.setVersion(0);
-        int i = animeInfo01Dao.updateById(animeInfo01);
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println(i>0);
-        System.out.println(i>0);
-        System.out.println(i>0);
+        animeInfo01.setVersion(1);
+        int i1 = animeInfo01Dao.updateById(animeInfo01);
+
 
     }
 
